748707bf04748ede24c8aa09d24a1b4066adf2ee
Suren <kumar.s.suren@gmail.com>
2026-02-27 16:03:47 -0500

    v2 start

--- interp/check_method.py
+++
@@ -1,4 +0,0 @@
-
-from transformer_lens import HookedTransformer
-print("Checking HookedTransformer methods...")
-print(f"Has set_use_attn_result: {hasattr(HookedTransformer, 'set_use_attn_result')}")
